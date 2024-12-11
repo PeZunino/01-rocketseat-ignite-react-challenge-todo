757f497362874515ac5af69126bf1785e504481f
PeZunino <pedrozsantos@hotmail.com>
2024-12-10 23:26:02 -0300

    page without tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,43 @@ import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
+import { PlusCircle } from "phosphor-react";
+import clipboard from "./assets/clipboard.svg";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Header />
+
       <main>
-        <Header />
+        <div>
+          <input placeholder="Adicionar uma nova tarefa"></input>
+          <button>
+            Criar
+            <PlusCircle size={16} />
+          </button>
+        </div>
+
+        <section>
+          <header>
+            <span>
+              Tarefas criadas <span>0</span>
+            </span>
+
+            <span>
+              Concluídas <span>0</span>
+            </span>
+          </header>
+
+          <div>
+            <img src={clipboard} alt="clipboard" />
+            <span>
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              <br />
+              Crie tarefas e organize seus itens a fazer
+            </span>
+          </div>
+        </section>
       </main>
       <GlobalStyle />
     </ThemeProvider>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,4 +17,99 @@ export const GlobalStyle = createGlobalStyle`
   body{
     background: ${(props) => props.theme["gray-600"]};
   }
+  
+  :focus {
+    outline: transparent;
+  }
+
+  main{
+    padding: 0rem 22rem ;
+  }
+  main > div{
+    height: 3.375rem;
+    gap: 0.5rem;
+    
+    display: flex;
+    margin-top: calc(0px - 1.6875rem);
+
+    input{
+      flex: 1;
+      background-color: ${(props) => props.theme["gray-500"]};
+      border-radius: 8px;
+      border: 1px solid ${(props) => props.theme["gray-700"]};;
+      padding: 1rem;
+    }
+    
+    button{
+      width: 5.625rem;
+      background-color: ${(props) => props.theme["blue-200"]};
+      color: ${(props) => props.theme["gray-100"]};
+      border-radius: 8px;
+      border: 0;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 0.5rem;
+    }
+  }
+
+  section{
+    margin-top: 4rem;
+
+  }
+
+  section > header{
+    font-weight: bold;
+    display: flex;
+    justify-content: space-between;
+    >span:nth-child(1){
+      color: ${(props) => props.theme["blue-100"]};
+    }
+    >span:nth-child(2){
+      color: ${(props) => props.theme["purple-100"]};
+    }
+    span > span{
+      background-color: ${(props) => props.theme["gray-400"]};
+      color: ${(props) => props.theme["gray-200"]};
+      border-radius: 999px;
+      font-size: 0.75rem ;
+      padding: 0.125rem 0.5rem;
+    }
+  }
+
+  section > div{
+    border-top: 1px solid ${(props) => props.theme["gray-400"]};
+    border-radius: 8px;
+    margin-top:1.5rem;
+    color: ${(props) => props.theme["gray-300"]};
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img{
+      margin-top: 4rem;
+      margin-bottom: 1rem;
+      height: 3.5rem;
+    }
+  }
+  /* main > div {
+    display:flex;
+    
+    margin: 0 auto;
+    gap: 0.5rem;
+
+    height: 3.375rem;
+    width: 46rem;
+    
+    background-color: ${(props) => props.theme["gray-500"]};
+  }
+    
+  input{
+    flex:1;
+  }
+  button{
+    width: 5.625rem;
+  } */
+  /* border:0; */
 `;
